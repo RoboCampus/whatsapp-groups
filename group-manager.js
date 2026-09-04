1a50ae24cc4528fbf0be5bb419193c48abb4bf96
unknown <liran12vernik@gmail.com>
2026-09-04 12:06:37 +0300

    niggers

--- group-manager.js
+++ group-manager.js
@@ -138,7 +138,13 @@ const app = express();
 app.use(express.json());
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
+    }
 });
 
 let isClientReady = false;
